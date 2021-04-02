9ecb97ad20ae289f67482dea1d7e75b49991f699
Md. Shakil Mahmud <mdshakilmahmud517@gmail.com>
2021-04-03 00:17:01 +0600

    import and variable uses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import TableComponent from './components/table'
+
 function App() {
+
+  var name = "Shakil";
+  var title = "Shakil is here";
+
+  var Link = {
+    text: "Click Here",
+    url:"https://google.com"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+    <h1>{name}</h1>
+    <p>{title}</p>
+
+    <a href={Link.url} target="_blank">{Link.text}</a>
+
+    <TableComponent/>
+  
     </div>
   );
 }
---
+++ src/components/table.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+function Table() {
+    return (
+        <div>
+            <table border="1">
+                <tr>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                </tr>
+                <tr>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                    <td>shakil</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+
+
+export default Table;
