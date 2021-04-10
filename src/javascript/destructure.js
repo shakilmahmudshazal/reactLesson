ba62ea988640807c8838cf4377493a38ac25272a
Md. Shakil Mahmud <mdshakilmahmud517@gmail.com>
2021-04-10 21:05:09 +0600

    js adedd with destructuring

---
+++ src/javascript/destructure.js
@@ -0,0 +1,52 @@
+// var people={
+//     name:"shakil",
+//     age:"26",
+//     gender:"male",
+// }
+
+
+// const {name, age, gender} = people;
+
+
+// console.log(name)
+// console.log(age)
+
+// var arr =["shakil", "26", "male"];
+
+// console.log(arr[0]);
+
+// const [name,age, gender]=arr;
+
+// console.log(name);
+
+// people =()=>{
+//     var obj = {
+//             name:"shakil2",
+//             age:"26",
+//             gender:"male",
+//         }
+
+//         return obj;
+// }
+
+
+// const {name}= people();
+
+// console.log(name)
+
+// people =()=>{
+
+//     var arr = ["shakil","26","male"];
+//     return arr;
+// }
+
+
+// const [name, age]= people();
+
+// console.log(name)
+
+// const [name, setName]= useState(2)
+// setName(5)
+
+
+
